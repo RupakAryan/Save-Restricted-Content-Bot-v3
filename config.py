7860346de6da0236c8cafca6ceb288fe0dd10eab
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-09-06 20:02:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,9 +23,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "8484698189:AAGISMT50yYSNyUlUivdclcoNjKNOVnCY
 MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://rupakaryanaryan:rupakaryanaryan@cluster0.05xkjx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "5776977809").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
-STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
+STRING = os.getenv("STRING", "BQHJcRUAtGxk7IAORf8Mw7iv263Gd1mJ1Pb6UiPx5JdQcEQGhKaDhpUQJuXAqLShyI9O-234K7w_wLoXxJt-GRgWqgpjRimAFON-JkmRo15Zqgwgt8UwBAcFeiI_Cu2cG7m58dY9ki0v5YzAT7qmPWotnFDMYWHVs-U6kSUVgc1fnlYF_DS0xneqJ3EAQPu5G-CoNG-adWMZstjAySulhMzwsoMBMihgAafUQ4kt19Epb2e3R41SwjR3S2aALFcmknu10bUyskqKau9HeKMqjYE1Qos2H4ilImU8jVNVZI3489t6xRY_m9k5SzVSVtdkL97cMEQqX1jOmXC8pxaZgFu29J1AyAAAAAFYVa-RAA") # optional
+LOG_GROUP = int(os.getenv("LOG_GROUP", "")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
@@ -34,5 +34,6 @@ FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+
 
 
